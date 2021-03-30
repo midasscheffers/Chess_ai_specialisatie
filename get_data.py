6cfb2c81889ee8e75991024d710cd7239cff5799
Midas Scheffers <midasscheffers@gmail.com>
2021-03-30 15:18:51 +0200

    get corretct net ipnut from dataget calss

--- get_data.py
+++ get_data.py
@@ -2,6 +2,8 @@ import chess
 import chess.engine
 import random as r
 
+piece_val_table = {"k":1.0, "q":0.7, "r":0.5, "b":0.3, "n":0.2, "p":0.1, ".":0.0, "K":-1.0, "Q":-0.7, "R":-0.5, "B":-0.3, "N":-0.2, "P":-0.1}
+
 class ChessData:
     def __init__(self):
         self.engine = chess.engine.SimpleEngine.popen_uci("content/stockfish")
@@ -22,7 +24,11 @@ class ChessData:
         for i in range(size):
             b = self.random_board()
             limit = chess.engine.Limit(time=time_limit)
-            data.append([b, self.engine.play(b,limit).move])
+
+            result = self.engine.analyse(b, chess.engine.Limit(depth=10))
+            score = result['score'].white().score()
+
+            data.append([self.board_to_ai_inp(b), self.move_to_ai_out(self.engine.play(b,limit).move), score])
         return data
     
 
@@ -32,10 +38,17 @@ class ChessData:
         return score
     
     def move_to_ai_out(self, move):
-        pass
+        m = str(move)
+        out = []
+        return out
 
     def board_to_ai_inp(self, board):
-        pass
+        b = str(board)
+        inp = []
+        for i in range(len(b)):
+            if b[i] in piece_val_table:
+                inp.append(piece_val_table[b[i]])
+        return inp
 
 
 dataGen = ChessData()
